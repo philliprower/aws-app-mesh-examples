92d96ffe12c604d88bb2f8837ad0632859bb0b28
Phillip Rower <phillip.rower@presidiohealth.com>
2019-07-29 14:03:06 -0700

    Update template to match recent changes on aws-app-mesh-inject.  According to AWS Support https://github.com/aws/aws-app-mesh-inject/issues/63 maybe issue with injector we've been facing.

--- examples/apps/djapp/2_create_injector/ca-bundle.sh
+++ examples/apps/djapp/2_create_injector/ca-bundle.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o pipefail
 
 export MESH_NAME=dj-app
-export IMAGE_NAME=602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-app-mesh-inject:v0.1.0
+export IMAGE_NAME=602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-app-mesh-inject:v0.1.5
 export MESH_REGION="" # Leave this empty
 
 ROOT=$(cd $(dirname $0)/; pwd)
